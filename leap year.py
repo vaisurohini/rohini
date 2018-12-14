b9971d56731c0a57fe78f595120e015ddb5cd991
vaisurohini <45839323+vaisurohini@users.noreply.github.com>
2018-12-14 14:32:14 +0530

    Create leap year.py

---
+++ leap year.py
@@ -0,0 +1,8 @@
+if year % 4 == 0 and year % 100 != 0:
+    print(enter the year, "is a Leap Year")
+elif year % 100 == 0:
+    print(enter the year, "is not a Leap Year")
+elif year % 400 ==0:
+    print(enter the year, "is a Leap Year")
+else:
+    print(enter the year, "is not a Leap Year")
