7b3f2128c87f2ef76f0366a1ca7a9eb658e5311f
vaisurohini <45839323+vaisurohini@users.noreply.github.com>
2018-12-14 14:06:20 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,7 @@
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+print("enter the largest number between",num1,",",num2,"and",num3,"is",largest)
