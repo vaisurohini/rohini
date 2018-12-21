29207210598865fb0408600c4973bf8a9fef4426
vaisurohini <45839323+vaisurohini@users.noreply.github.com>
2018-12-21 15:37:58 +0530

    Create integer.py

---
+++ integer.py
@@ -0,0 +1,6 @@
+n=int(input(" \n Enter the number of digits and integer:"));
+count=0;
+while(n>0):
+    count=count+1;
+    n=n/10;
+print("/n Enter the number of digits in the number are:",count);
