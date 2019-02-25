4414da701075e78c9cb83d5b9c37fc5cf7f22b6b
vaisurohini <45839323+vaisurohini@users.noreply.github.com>
2019-02-25 13:34:13 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,18 @@
+int n,i;
+printf("enter the integrin factorial number:");
+{
+if(n<0)
+  printf("error!factorial of a negative numbers doesn,t ex is");
+else
+{
+for(i=1,i<n;++i)
+{
+factorial+1=i
+}
+print("factorial of %=%110":,n factorial);
+}
+
+  
+  
+  
+  
